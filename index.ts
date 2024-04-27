6003da89f7e911ba88a1a5040b1eecfbed20f68b
Yukai Huang <yukaihuangtw@gmail.com>
2024-04-27 18:39:34 +0800

    feat: persistent and simple create

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
+import * as pulumi from "@pulumi/pulumi";
 import { LocalWorkspace } from "@pulumi/pulumi/automation";
 import * as vultr from "@ediri/vultr";
 import dotenv from "dotenv";
+import { cloudConfigString } from "./cloud-config";
+import { nanoid } from "nanoid"
 
 dotenv.config();
-
-import { cloudConfigString } from "./cloud-config";
 
 const args = process.argv.slice(2);
 let destroy = false;
@@ -12,35 +13,81 @@ if (args.length > 0 && args[0]) {
   destroy = args[0] === "destroy";
 }
 
+let create = false
+if (args.length > 0 && args[0]) {
+  create = args[0] === "create";
+}
+
+const userData = cloudConfigString.replace(
+  /\$TS_AUTH_KEY/,
+  process.env.TS_AUTH_KEY!,
+);
+
 // const regionsResult = (await fetch("https://api.vultr.com/v2/regions").then(
 //   (r) => r.json(),
 // )) as { regions: vultr.GetRegionResult[] };
 
+let stack: pulumi.automation.Stack;
 
-async function program() {
-  const userData = cloudConfigString.replace(
-    /\$TS_AUTH_KEY/,
-    process.env.TS_AUTH_KEY!,
-  );
+let instancesInfo: InstanceInfo[] = [];
 
-  const instanceResource = new vultr.Instance("instanceResource", {
-    // alpine
-    osId: 2136,
-    plan: "vc2-1c-1gb",
-    region: "sgp",
-    backups: "disabled",
-    userData,
-    activationEmail: false,
-  });
-
-  return {
-    hostname: instanceResource.hostname,
+type InstanceInfo = {
+  name: string;
+  id: string;
+  hostname: string;
+  ip: string;
+};
+type StackOutput = {
+  [key: number]: {
+    value: InstanceInfo;
   };
+};
+
+function mapStackOutputToArray(output: StackOutput): InstanceInfo[] {
+  return Object.values(output).map((v) => v.value);
 }
 
+const getProgram = () => async () => {
+  const existing = instancesInfo.map((info) => {
+    const instance = vultr.Instance.get(info.name, info.id);
+
+    return {
+      name: pulumi.Output.create(info.name),
+      id: instance.id,
+      hostname: instance.hostname,
+      mainIp: instance.mainIp,
+    };
+  });
+
+  const newInstances = []
+  // create new ones
+  if (create) {
+    const name = `vultrInstance-${nanoid(5)}`
+    const instance = new vultr.Instance(name, {
+      // alpine
+      osId: 2136,
+      plan: "vc2-1c-1gb",
+      region: "sgp",
+      backups: "disabled",
+      userData,
+      activationEmail: false,
+      label: "tailportal",
+      tags: ["tailportal"],
+    });
+    newInstances.push({
+      name: pulumi.Output.create(name),
+      id: instance.id,
+      hostname: instance.hostname,
+      mainIp: instance.mainIp,
+    })
+  }
+
+  return [...existing, ...newInstances];
+};
+
 async function main() {
-  const stack = await LocalWorkspace.createOrSelectStack({
-    program,
+  stack = await LocalWorkspace.createOrSelectStack({
+    program: getProgram(),
     projectName: "tail-portal",
     stackName: "dev",
   });
@@ -54,6 +101,9 @@ async function main() {
     console.info("stack destroy complete");
     process.exit(0);
   }
+
+  // set existing outputs
+  instancesInfo = mapStackOutputToArray(await stack.outputs());
 
   await stack.up({ onOutput: console.log });
 }
